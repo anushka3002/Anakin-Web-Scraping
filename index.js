a7dee523e800172bfb515079dfafaeb31be3f896
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-06-24 07:39:53 +0530

    checking puppeteer

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://en.wikipedia.org/wiki/Ratan_Tata');
   await page.screenshot({path: 'wiki.png'});
-  await browser.waitForTarget(()=>false)
+//   await browser.waitForTarget(()=>false)
+
+  const result=await page.evaluate(()=>{
+    let headingFromWeb = document.querySelectorAll(".mw-headline")
+    const headingList=[...headingFromWeb]
+    return headingList.map(h=>h.innerText)
+  })
+
+  console.log(result)
+
   await browser.close();
 })();
