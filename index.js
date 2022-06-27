ba135462da390bc4a4a194faddc3dfc1e6e73c43
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-06-27 21:52:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-var final
 const puppeteer = require('puppeteer');
-
 
 (async () => {
   const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
-  await page.goto('https://en.wikipedia.org/wiki/Ratan_Tata');
+  await page.goto('https://food.grab.com/sg/en/restaurants');
   await page.screenshot({path: 'wiki.png'});
-//   await browser.waitForTarget(()=>false)
+  await browser.waitForTarget(()=>false)
 
-  let result=await page.evaluate(()=>{
-    let headingFromWeb = document.querySelectorAll(".mw-headline")
-    const headingList=[...headingFromWeb]
-    return headingList.map(h=>h.innerText)
+  const result=await page.evaluate(()=>{
+    const data = JSON.parse(document.getElementById("__NEXT_DATA__").textContent)
+      let restaurantList = data.props.initialReduxState.pageRestaurantsV2.entities.restaurantList
+      for (let key in restaurantList){
+         return ({latitude: restaurantList[key].latitude,
+          longitude: restaurantList[key].longitude})
+      }
+    
   })
-  final=result
-  console.log(final)
-  
-  // document.querySelector("#name").append(result)
+  console.log(result)
 
   await browser.close();
 })();
 
-console.log("hello"+final+"final")
 
-
-// function getLatLong(){
+// function getData(){
 //   const data = JSON.parse(document.getElementById("__NEXT_DATA__").textContent)
-//   let restaurantList = data.props.initialReduxState.pageRestaurantsV2.entities.restaurantList
-//   for (let key in restaurantList){
-//      return ({latitude: restaurantList[key].latitude,
-//       longitude: restaurantList[key].longitude})
-//   }
+//   console.log(data+"data")
+//       let restaurantList = data.props.initialReduxState.pageRestaurantsV2.entities.restaurantList
+//       for (let key in restaurantList){
+//          return ({latitude: restaurantList[key].latitude,
+//           longitude: restaurantList[key].longitude})
+//       }
 // }
-// console.log(getLatLong())
+// console.log(getData())
